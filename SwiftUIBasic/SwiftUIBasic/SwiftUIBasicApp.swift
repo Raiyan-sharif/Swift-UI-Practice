e86ebae7ca378019923f4502abfa9c37e376eb5c
Raiyan Sharif <raiyan.sharif@bjitgroup.com>
2023-01-30 17:14:36 +0600

    git ignore file added

---
+++ SwiftUIBasic/SwiftUIBasic/SwiftUIBasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBasicApp.swift
+//  SwiftUIBasic
+//
+//  Created by BJIT on 30/1/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
