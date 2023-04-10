265e0f6be270dcde9f5b1fb84a79498127fb21ca
Raiyan Sharif <raiyan.sharif@bjitgroup.com>
2023-04-10 09:50:13 +0600

    Swift UI

---
+++ pushNotificationDemo/pushNotificationDemo/pushNotificationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  pushNotificationDemoApp.swift
+//  pushNotificationDemo
+//
+//  Created by BJIT on 27/2/23.
+//
+
+import SwiftUI
+
+@main
+struct pushNotificationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
