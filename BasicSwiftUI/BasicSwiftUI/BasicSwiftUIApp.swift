62b61e5ff3edc76c34b476758dc0341530e0ac6c
Raiyan Sharif <raiyan.sharif@bjitgroup.com>
2023-06-14 09:52:29 +0600

    project init

---
+++ BasicSwiftUI/BasicSwiftUI/BasicSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicSwiftUIApp.swift
+//  BasicSwiftUI
+//
+//  Created by BJIT on 14/6/23.
+//
+
+import SwiftUI
+
+@main
+struct BasicSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
