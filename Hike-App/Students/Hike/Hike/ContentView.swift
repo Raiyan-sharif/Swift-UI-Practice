43c65f233c96cc57a81741b42fa553fa73675684
Raiyan Sharif <raiyan.sharif@bjitgroup.com>
2023-08-30 17:54:20 +0600

    Hiking App CardView

---
+++ Hike-App/Students/Hike/Hike/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Hike
+//
+//  Created by Raiyan Sharif on 30/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        CardView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hike-App/Students/Hike/Hike/Utility/ColorExtension.swift
@@ -0,0 +1,20 @@
+//
+//  ColorExtension.swift
+//  Hike
+//
+//  Created by BJIT on 30/8/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color{
+    static let customGreenLight = Color("ColorGreenLight")
+    static let customGreenMedium = Color("ColorGreenMedium")
+    static let customGreenDark = Color("ColorGreenDark")
+    static let customGrayLight = Color("ColorGrayLight")
+    static let customGrayMedium = Color("ColorGrayMedium")
+    static let customIndigoMedium = Color("ColorIndigoMedium")
+    static let customSalmonLight = Color("ColorSalmonLight")
+
+}
