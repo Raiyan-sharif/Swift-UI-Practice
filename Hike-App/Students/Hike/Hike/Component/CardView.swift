e6139f5cbd07f7efb325c9202ca607e25b32dc0a
Raiyan Sharif <raiyan.sharif@bjitgroup.com>
2023-08-31 12:32:45 +0600

    Motion Animation Added

--- Hike-App/Students/Hike/Hike/Component/CardView.swift
+++ Hike-App/Students/Hike/Hike/Component/CardView.swift
@@ -8,6 +8,17 @@
 import SwiftUI
 
 struct CardView: View {
+
+    @State private var imageNumber: Int = 1
+    @State private var randomNumber: Int = 1
+
+    func randomImage(){
+        repeat{
+            randomNumber = Int.random(in: 1...5)
+        }while randomNumber == imageNumber
+        imageNumber = randomNumber
+    }
+
     var body: some View {
         ZStack {
             CustomBackgroundView()
@@ -40,15 +51,26 @@ struct CardView: View {
 
                 //MARK: - MAIN CONTENT
                 ZStack {
-                    Circle()
-                        .fill(LinearGradient(colors: [Color("ColorIndigoMedium"), Color("ColorSalmonLight")], startPoint: .topLeading, endPoint: .bottomTrailing))
-                        .frame(width: 256, height: 256)
-                    Image("image-1")
+                    CustomCircleView()
+                    Image("image-\(imageNumber)")
                         .resizable()
                         .scaledToFit()
+                        .animation(.default, value: imageNumber)
 
                 }
                 //MARK: - FOOTER
+                Button{
+                    print("The Button was pressed.")
+                    randomImage()
+                }label: {
+                    Text("Explore More")
+                        .font(.title2)
+                        .fontWeight(.heavy)
+                        .foregroundStyle(LinearGradient(colors: [.customGreenLight, .customGreenMedium], startPoint: .top, endPoint: .bottom))
+                        .shadow(color: .black.opacity(0.25), radius: 0.25,x: 1, y: 2)
+                }
+                .buttonStyle(GradientButton())
+
             }
         } //: CARD
         .frame(width: 320, height: 570)
