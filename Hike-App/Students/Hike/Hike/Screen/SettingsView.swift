1858f38a0642df8ebeb2761cfc09349aaedb3a9b
Raiyan Sharif <raiyan.sharif@bjitgroup.com>
2023-09-01 12:31:16 +0600

    App icon Alternate

--- Hike-App/Students/Hike/Hike/Screen/SettingsView.swift
+++ Hike-App/Students/Hike/Hike/Screen/SettingsView.swift
@@ -8,6 +8,17 @@
 import SwiftUI
 
 struct SettingsView: View {
+    //MARK: - Properties
+
+    private let alternateAppIcons: [String] = [
+        "AppIcon-MagnifyingGlass",
+        "AppIcon-Map",
+        "AppIcon-Mushroom",
+        "AppIcon-Camera",
+        "AppIcon-Backpack",
+        "AppIcon-Campfire"
+    ]
+
     var body: some View {
         List{
             // MARK: - SECTION: HEADER
@@ -49,6 +60,41 @@ struct SettingsView: View {
 
             // MARK: - SECTION: ICONS
 
+            Section(header: Text("Alternate Icons")) {
+                ScrollView(.horizontal, showsIndicators: false){
+                    HStack (spacing: 12) {
+                        ForEach(alternateAppIcons.indices, id: \.self) { item in
+                            Button{
+                                print("Icon was pressed.")
+                                UIApplication.shared.setAlternateIconName(alternateAppIcons[item]){
+                                    error in
+                                    if error != nil{
+                                        print("Failed request to update the app's icon: \(String(describing: error?.localizedDescription))")
+                                    }else{
+                                        print("Success! You have changed the app's icon to \(alternateAppIcons[item])")
+                                    }
+                                }
+                            }label: {
+                                Image("\(alternateAppIcons[item])-Preview")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 80,height: 80)
+                                    .cornerRadius(16)
+                            }
+                        .buttonStyle(.borderless)
+                        }
+                    }
+                }
+                .padding(.top,12)
+                Text("Choose your favourite app icon from the collection above.")
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.secondary)
+                    .font(.footnote)
+                    .padding(.bottom,12)
+            }
+            .listRowSeparator(.hidden)
+
             // MARK: - SECTION: ABOUT
 
             Section(
@@ -62,26 +108,16 @@ struct SettingsView: View {
 
             ){
                 // 1. Basic Labeled Content
-                LabeledContent("Application", value: "Hike")
+               // LabeledContent("Application", value: "Hike")
 
                 // 2. Advanced Labeled Content
-                LabeledContent{
-                    Text("Hike")
-                        .foregroundColor(.primary)
-                        .fontWeight(.heavy)
-                }label: {
-                    ZStack{
-                        RoundedRectangle(cornerRadius: 8)
-                            .frame(width: 30, height: 30)
-                            .foregroundColor(.blue)
-                        Image(systemName: "apps.iphone")
-                            .foregroundColor(.white)
-                            .fontWeight(.semibold)
-                    }
-                    HStack {
-                        Text("Application")
-                    }
-                }
+                CustomListRowView(rowLabel: "Application", rowIcon: "apps.iphone", rowContent: "HIKE", rowTintColor: .blue)
+                CustomListRowView(rowLabel: "Compibility", rowIcon: "info.circle", rowContent: "iOS, iPadOS", rowTintColor: .red)
+                CustomListRowView(rowLabel: "Technology", rowIcon: "swift", rowContent: "Swift", rowTintColor: .orange)
+                CustomListRowView(rowLabel: "Version", rowIcon: "gear", rowContent: "1.0", rowTintColor: .purple)
+                CustomListRowView(rowLabel: "Developer", rowIcon: "ellipsis.curlybraces", rowContent: "Johm Doe", rowTintColor: .mint)
+                CustomListRowView(rowLabel: "Designer", rowIcon: "paintpalette", rowContent: "Robert Petras", rowTintColor: .pink)
+                CustomListRowView(rowLabel: "Website", rowIcon: "globe", rowTintColor: .indigo, rowLinkLabel: "Credo Academy" ,rowLinkDestination: "https://credo.academy")
             }
 
         }// : List
